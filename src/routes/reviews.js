bb677e80ae0dd01298d7c6d0041a4282aa732307
Imtanan_Aziz_Toor <imtanan@smash.cloud>
2024-12-21 17:14:28 +0500

    W.I.P

--- src/routes/reviews.js
+++ src/routes/reviews.js
@@ -9,11 +9,39 @@ class ReviewsRoutes {
     setupRoutes() {
         this.router.get("/", this.getReviews);
         this.router.get("/product/:productId", this.getProductReviews);
+        this.router.get("/user/:userId", this.getUserReviews);
     }
     getReviews(_req, res) {
         res.status(200).send({
             message: "Hello from reviews route",
             data: [],
+        });
+    }
+    getUserReviews(req, res) {
+        res.status(200).send({
+            userId: req.params.userId,
+            reviews: [
+                {
+                    id: 1,
+                    productId: 1,
+                    images: [],
+                    rating: 4.5,
+                    comment: "Great product",
+                    reviewerId: 1,
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    id: 2,
+                    productId: 2,
+                    images: [],
+                    rating: 4.5,
+                    comment: "Great product",
+                    reviewerId: 1,
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+            ],
         });
     }
     getProductReviews(req, res) {
--- src/routes/reviews.ts
+++ src/routes/reviews.ts
@@ -10,12 +10,41 @@ class ReviewsRoutes {
   private setupRoutes(): void {
     this.router.get("/", this.getReviews);
     this.router.get("/product/:productId", this.getProductReviews);
+    this.router.get("/user/:userId", this.getUserReviews);
   }
 
   private getReviews(_req: Request, res: Response): void {
     res.status(200).send({
       message: "Hello from reviews route",
       data: [],
+    });
+  }
+
+  private getUserReviews(req: Request, res: Response) {
+    res.status(200).send({
+      userId: req.params.userId,
+      reviews: [
+        {
+          id: 1,
+          productId: 1,
+          images: [],
+          rating: 4.5,
+          comment: "Great product",
+          reviewerId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 2,
+          productId: 2,
+          images: [],
+          rating: 4.5,
+          comment: "Great product",
+          reviewerId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
     });
   }
 
