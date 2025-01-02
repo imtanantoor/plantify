3e40fba8c0e3df55be9030a80de8e7314c797be3
Imtanan_Aziz_Toor <imtanan@smash.cloud>
2025-01-02 13:17:12 +0500

    w.i.p

--- src/utils/constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const PORT = process.env.PORT || 8000;
-const constants = {
-    PORT
-};
-exports.default = constants;
