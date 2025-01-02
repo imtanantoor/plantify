c4a7e9e1ee6aa2d7e0c0cde68a6982e5e8f84c8c
Imtanan_Aziz_Toor <imtanan@smash.cloud>
2025-01-02 14:29:46 +0500

    reviews controller w.i.p

--- src/controllers/reviews.controller.ts
+++ src/controllers/reviews.controller.ts
@@ -1,40 +1,39 @@
 import { Request, Response } from "express";
-
+import { Review } from "../models";
 class ReviewsController {
   async getReviews(req: Request, res: Response): Promise<void> {
-    res.status(200).send({ message: "Hello from review route" });
+    try {
+      const reviews = await Review.findMany();
+      res.status(200).send({ data: reviews });
+    } catch (error) {
+      res.status(500).send({ message: "Internal server error" });
+    }
   }
 
   async getProductReviews(req: Request, res: Response): Promise<void> {
-    res.status(200).send({ message: "Hello from product review route" });
+    try {
+      const reviews = await Review.findMany({
+        where: {
+          productId: parseInt(req.params.productId),
+        },
+      });
+      res.status(200).send({ data: reviews });
+    } catch (error) {
+      res.status(500).send({ message: "Internal server error" });
+    }
   }
 
   async getUserReviews(req: Request, res: Response) {
-    res.status(200).send({
-      userId: req.params.userId,
-      reviews: [
-        {
-          id: 1,
-          productId: 1,
-          images: [],
-          rating: 4.5,
-          comment: "Great product",
-          reviewerId: 1,
-          createdAt: new Date(),
-          updatedAt: new Date(),
+    try {
+      const reviews = await Review.findMany({
+        where: {
+          customerId: parseInt(req.params.userId),
         },
-        {
-          id: 2,
-          productId: 2,
-          images: [],
-          rating: 4.5,
-          comment: "Great product",
-          reviewerId: 1,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-      ],
-    });
+      });
+      res.status(200).send({ data: reviews });
+    } catch (error) {
+      res.status(500).send({ message: "Internal server error" });
+    }
   }
 }
 
--- src/models/index.ts
+++ src/models/index.ts
@@ -2,7 +2,7 @@ import prisma from "../../prisma/client";
 
 export const Categories = prisma.category;
 export const Products = prisma.product;
-export const Reviews = prisma.review;       
+export const Review = prisma.review;       
 export const Users = prisma.customer;
 export const Orders = prisma.order;
 export const Tags = prisma.tag;
