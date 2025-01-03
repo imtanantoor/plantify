b04f4441e5e48be743ed2e64a25c9967d1d8f223
Imtanan_Aziz_Toor <imtanan@smash.cloud>
2025-01-03 14:13:32 +0500

    products controller updated

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { Products } from "../models";
 
 class ProductsController {
   async getProduct(req: Request, res: Response): Promise<void> {
@@ -18,7 +19,21 @@ class ProductsController {
     });
   }
   async getProducts(req: Request, res: Response): Promise<void> {
-    res.status(200).send({ message: "Hello from product route" });
+    try {
+      const products = await Products.findMany({
+        take: 10,
+        skip: 0,
+      });
+      res.status(200).send({
+        message: "Products fetched successfully",
+        data: products,
+      });
+    } catch (error) {
+      res.status(500).send({
+        message: "Internal server error",
+        error: error,
+      });
+    }
   }
 }
 
